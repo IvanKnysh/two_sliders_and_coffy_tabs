05b0be4384e0b24738fce1bfa9a784bc3da84443
Ivan Knysh <mr.rauber.ik@gmail.com>
2022-06-07 16:09:43 +0300

    Task 2 -> added mousedown/mouseup event

--- task-2.js
+++ task-2.js
@@ -12,20 +12,31 @@ class SliderBeforeAfter {
     addRangeForEachSliders() {
         this.sliders.forEach(item => {
             item.insertAdjacentHTML('beforeend', `
-                <div class="range"><span>Потяни</span></div>
+                <div class="range"><span></span></div>
             `);
         });
     }
 
     range() {
+        let isMoving = false;
+
+        this.wrapper.addEventListener("mousedown", () => {
+            isMoving = true;
+        });
+
+        this.wrapper.addEventListener("mouseup", () => {
+            isMoving = false;
+        });
+
         this.sliders.forEach(item => {
-            item.addEventListener("mousemove", function (event) {
+            item.addEventListener("mousemove", (event) => {
                 const target = event.target;
                 const cursorPositionInPercent = event.offsetX * 100 / 960;
 
-                target.closest('.slider').querySelector('img').style.clipPath = `inset(0 0 0 ${cursorPositionInPercent}%)`;
-
-                target.closest('.slider').querySelector('.range').style.left = cursorPositionInPercent + '%';
+                if (isMoving === true) {
+                    target.closest('.slider').querySelector('img').style.clipPath = `inset(0 0 0 ${cursorPositionInPercent}%)`;
+                    target.closest('.slider').querySelector('.range').style.left = cursorPositionInPercent + '%';
+                }
             });
         });
     }
