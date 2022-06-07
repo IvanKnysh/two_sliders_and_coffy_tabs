c88c219804e3105d3fc71cffac10308610edf6dd
Ivan Knysh <mr.rauber.ik@gmail.com>
2022-06-07 16:14:01 +0300

    Task 3 -> init

---
+++ task-3.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/*
+* Завдання 3 Інтерфейс кавового автомата
+* */
+class CoffeeMachine {
+    constructor() {
+
+    }
+
+    init() {
+        console.log(this);
+    }
+}
+
+const coffeeMachine = new CoffeeMachine();
+coffeeMachine.init();
