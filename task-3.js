18bbed30deeeb2fb91589fb25195815c076737d5
ivan <mr.rauber.ik@gmail.com>
2022-06-09 12:07:16 +0300

    Added CSS

--- task-3.js
+++ task-3.js
@@ -5,11 +5,31 @@
 * */
 class CoffeeMachine {
     constructor() {
+        this.wrapper = document.querySelector('.coffy');
+        this.ul = this.wrapper.querySelector('ul');
+    }
 
+    addDefaultMenu(data) {
+        let items = '';
+
+        data.forEach(item => {
+            items += `<li><span>${item.title_ua}</span></li>`;
+        });
+
+        this.ul.insertAdjacentHTML('beforeend', items);
+    }
+
+    getData() {
+        fetch('./recept.json')
+            .then(response => response.json())
+            .then(data => {
+                this.addDefaultMenu(data);
+            })
+            .catch(error => console.error(error));
     }
 
     init() {
-        console.log(this);
+        this.getData();
     }
 }
 
