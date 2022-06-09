272f9ddda67f1c4ed5cfc801f4a84ada991b42ba
ivan <mr.rauber.ik@gmail.com>
2022-06-09 14:53:53 +0300

    Task 3 -> CoffeeMachine development completed

--- task-3.js
+++ task-3.js
@@ -9,7 +9,7 @@ class CoffeeMachine {
         this.ul = this.wrapper.querySelector('ul');
     }
 
-    addDefaultMenu(data) {
+    addDefaultMenuItems(data) {
         let items = '';
 
         data.forEach(item => {
@@ -19,13 +19,35 @@ class CoffeeMachine {
         this.ul.insertAdjacentHTML('beforeend', items);
     }
 
+    addMenuItemActiveClass(target) {
+        this.ul.querySelectorAll('li').forEach(item => {
+            item.classList.remove('active');
+        });
+
+        target.parentElement.classList.add('active');
+    }
+
+    clearSpan() {
+        this.wrapper.querySelector('.coffe-content').querySelectorAll('span').forEach(item => {
+            item.removeAttribute('class');
+            item.removeAttribute('style');
+        });
+    }
+
     menuClick(data) {
         this.ul.addEventListener('click', (e) => {
             const target = e.target;
+            const countElements = this.wrapper.querySelector('.coffe-content').childElementCount;
+
+            this.addMenuItemActiveClass(target);
+            this.clearSpan();
 
             data.forEach(item => {
                 if (target.parentElement.dataset.name === item.title_en) {
-                    console.log(item.recipe);
+                    item.recipe.forEach((rec, i) => {
+                        this.wrapper.querySelector('.coffe-content').children[i].classList.add(rec.class_name);
+                        this.wrapper.querySelector('.coffe-content').children[i].style.height = rec.volume / countElements * 100 + '%';
+                    });
                 }
             });
         });
@@ -35,7 +57,7 @@ class CoffeeMachine {
         fetch('./recept.json')
             .then(response => response.json())
             .then(data => {
-                this.addDefaultMenu(data);
+                this.addDefaultMenuItems(data);
                 this.menuClick(data);
             })
             .catch(error => console.error(error));
